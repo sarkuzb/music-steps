64f77dcbc54e5311112bc9c362abd7f0ee498c94
sarkuzb <ssmartprinter@gmail.com>
2025-07-09 00:53:43 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import MusicPlayer from "./Music_Player";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <MusicPlayer />
+    </>
+  );
+}
+
+export default App;
---
+++ src/data.jsx
@@ -0,0 +1,84 @@
+// data.jsx
+import song1 from "./assets/something1.mp3";
+import song2 from "./assets/something2.mp3";
+import song3 from "./assets/something3.mp3";
+import song4 from "./assets/something4.mp3";
+import song5 from "./assets/something5.mp3";
+import song6 from "./assets/something6.mp3";
+import song7 from "./assets/something7.mp3";
+import song8 from "./assets/something8.mp3";
+import song9 from "./assets/something9.mp3";
+import song10 from "./assets/something10.mp3";
+import song11 from "./assets/something11.mp3";
+import song12 from "./assets/something12.mp3";
+import song13 from "./assets/something13.mp3";
+
+const musicLibrary = [
+  {
+    title: "Kunlarning Birida",
+    artist: "Sarvar Khujamov",
+    file: song1,
+  },
+  {
+    title: "Jimgina",
+    artist: "Sarvar Khujamov",
+    file: song2,
+  },
+  {
+    title: "Qilaman Sayohat",
+    artist: "Sarvar Khujamov",
+    file: song3,
+  },
+  {
+    title: "Onajonim",
+    artist: "Sarvar Khujamov",
+    file: song4,
+  },
+  {
+    title: "Bilmadim",
+    artist: "Sarvar Khujamov",
+    file: song5,
+  },
+  {
+    title: "Echos",
+    artist: "Sarvar Khujamov",
+    file: song6,
+  },
+  {
+    title: "Men va U",
+    artist: "Sarvar Khujamov",
+    file: song7,
+  },
+  {
+    title: "Moments",
+    artist: "Sarvar Khujamov",
+    file: song8,
+  },
+  {
+    title: "Yuksalish",
+    artist: "Sarvar Khujamov",
+    file: song9,
+  },
+  {
+    title: "Lil Bro",
+    artist: "Sarvar Khujamov",
+    file: song10,
+  },
+  {
+    title: "Qadamlar",
+    artist: "Sarvar Khujamov",
+    file: song11,
+  },
+  {
+    title: "Arabic Nights",
+    artist: "Sarvar Khujamov",
+    file: song12,
+  },
+  {
+    title: "Life Is Not A Game",
+    artist: "Sarvar Khujamov",
+    file: song13,
+  },
+];
+
+export default musicLibrary;
